e8c96432578bbe8477c28f6e6a7c069de8d7b754
Bill Mei <kortaggio@users.noreply.github.com>
2015-04-22 14:10:53 -0400

    css colors and more logic

--- js/views.js
+++ js/views.js
@@ -2,17 +2,19 @@ function listBadges() {
 
 }
 
-function addLinkedInButton(course) {
-	var $badgesList = $('#badges-list');
+function displayCourse(courseName, url) {
+  var $badgesList = $('#badges-list');
 
-	var view = {
-		course: course,
-	};
-
-	var template = '{{badge}} <img src="{{src}}" />';
-	var html = Mustache.to_html(template, view);
-	$badgesList.html(html);
-
+  var view = {
+    course: courseName,
+    share_url : url
+  };
+  
+  var template = '<a href="{{share_url}}" rel="nofollow">' +
+                 '<span class="course-title">{{course}}</span>' +
+                 '<img src="img/linkedin-add-to-profile.png" alt="Add To LinkedIn" class="btn-linkedin"/></a>';
+  var html = Mustache.to_html(template, view);
+  $badgesList.html(html);
 }
 
 
